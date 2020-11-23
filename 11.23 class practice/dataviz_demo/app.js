c7e86d68ef0556dc0955fcf4e1e4227da406f991
Gong Yuqing <70460683+GongYuqing@users.noreply.github.com>
2020-11-23 19:10:40 +0800

    Update app.js

--- 11.23 class practice/dataviz_demo/app.js
+++ 11.23 class practice/dataviz_demo/app.js
@@ -7,7 +7,7 @@ var map = L.map('map')
  layerGroup = L.layerGroup().addTo(map);
 
  var items = [];
- var airtable_read_endpoint = "https://api.airtable.com/v0/appKIU0zkdHt3AVTL/Venues?api_key=keyc0wQXlkrsrlP15";
+ var airtable_read_endpoint = "https://api.airtable.com/v0/appIYCvOKbLeoCE7Y/Product?api_key=keyc0wQXlkrsrlP15";
  var data = [];
  $.getJSON(airtable_read_endpoint, function(result) {
        $.each(result.records, function(key,value) {
@@ -41,7 +41,7 @@ function clear_map() {
     layerGroup = L.layerGroup().addTo(map);
 
     var items = [];
-    var airtable_read_endpoint = "https://api.airtable.com/v0/appKIU0zkdHt3AVTL/Venues?api_key=keyc0wQXlkrsrlP15";
+    var airtable_read_endpoint = "https://api.airtable.com/v0/appIYCvOKbLeoCE7Y/Product?api_key=keyc0wQXlkrsrlP15";
     var data = [];
     $.getJSON(airtable_read_endpoint, function(result) {
           $.each(result.records, function(key,value) {
@@ -93,7 +93,7 @@ $(document).ready(function(){
       var items = [];
       var i = 0;
       $('.table-responsive').show();
-      var airtable_read_endpoint = "https://api.airtable.com/v0/appKIU0zkdHt3AVTL/Roll-up?api_key=keyc0wQXlkrsrlP15";
+      var airtable_read_endpoint = "https://api.airtable.com/v0/appIYCvOKbLeoCE7Y/Product?api_key=keyc0wQXlkrsrlP15";
       $.getJSON(airtable_read_endpoint, function(result) {
             $.each(result.records, function(key,value) {
                 items = [];
@@ -123,7 +123,7 @@ $(document).ready(function(){
         var items = [];
         var i = 0;
         $('.table-responsive').show();
-        var airtable_read_endpoint = "https://api.airtable.com/v0/appKIU0zkdHt3AVTL/Roll-up?api_key=keyc0wQXlkrsrlP15";
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appIYCvOKbLeoCE7Y/Product?api_key=keyc0wQXlkrsrlP15";
         $.getJSON(airtable_read_endpoint, function(result) {
               $.each(result.records, function(key,value) {
                   items = [];
